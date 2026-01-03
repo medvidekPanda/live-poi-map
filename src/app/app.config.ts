d85c69b7ba79b3f8a8d52b143f67a8f05f280c9e
Jan Ptáček <honza@fogll.net>
2026-01-03 08:37:34 +0100

    Add PrimeNG configuration and project setup
    - Add PrimeNG and PrimeUI themes
    - Configure ESLint and Prettier
    - Update app configuration with PrimeNG provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from "@angular/core";
+import { provideClientHydration, withEventReplay } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
 
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Aura from "@primeuix/themes/aura";
+import { providePrimeNG } from "primeng/config";
+
+import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+  ],
 };
